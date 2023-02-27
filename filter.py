1f0b1743ad66d5381d1b8701bae273bc7f18eafe
harsha17116031 <ghantaharsha.harshareddy@gmail.com>
2023-02-27 10:45:46 -0600

    2

--- filter.py
+++ filter.py
@@ -1,26 +1,28 @@
 ratings={}
 mean_of_users={}
+mean_centered_ratings={}
 
-with open('./netflix/TestingRatings.txt') as f:
+with open('./netflix/TrainingRatings.txt') as f:
     lines = f.readlines()
 
 for i in lines:
     temp=i.replace("\n","").split(',')
-    if not temp[0] in ratings.keys():
-        ratings[temp[0]]={}
+    if not temp[1] in ratings.keys():
+        ratings[temp[1]]={}
 
-    ratings[temp[0]][temp[1]]=temp[2]
+    ratings[temp[1]][temp[0]]=float(temp[2])
 
 
 for i in ratings.keys():
     sum_of_user_ratings=0
     count=0
     for j in ratings[i].keys():
-        sum_of_user_ratings+=float(ratings[i][j])
+        sum_of_user_ratings+=ratings[i][j]
         count+=1
     
     mean_of_users[i]=sum_of_user_ratings/count
 
+    for j in ratings[i].keys():
+        ratings[i][j]=ratings[i][j]-mean_of_users[i]
 
 
-print(mean_of_users)
