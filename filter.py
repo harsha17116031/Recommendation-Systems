5ef0f6bfe5d1319359cbefd1e56eea03a39586e2
harsha17116031 <ghantaharsha.harshareddy@gmail.com>
2023-02-27 13:46:45 -0600

    4

--- filter.py
+++ filter.py
@@ -1,55 +1,4 @@
-# ratings={}
-# mean_of_users={}
-# mean_centered_ratings={}
-# weights_of_users={}
-
-# with open('./netflix/TrainingRatings.txt') as f:
-#     lines = f.readlines()
-
-# for i in lines:
-#     temp=i.replace("\n","").split(',')
-#     if not temp[1] in ratings.keys():
-#         ratings[temp[1]]={}
-
-#     ratings[temp[1]][temp[0]]=float(temp[2])
-
-
-# for i in ratings.keys():
-#     sum_of_user_ratings=0
-#     count=0
-#     for j in ratings[i].keys():
-#         sum_of_user_ratings+=ratings[i][j]
-#         count+=1
-    
-#     mean_of_users[i]=sum_of_user_ratings/count
-
-#     for j in ratings[i].keys():
-#         ratings[i][j]=ratings[i][j]-mean_of_users[i]
-
-
-# calculating weights of users
-
-def find_similar_movie_ratings(first_user,second_user):
-    common_movie_ratings=[]
-    if len(first_user.keys())>len(second_user.keys()):
-        for i in second_user.keys():
-            if i in first_user.keys():
-                common_movie_ratings.append([first_user[i],second_user[i]])
-    else:
-        for i in first_user.keys():
-            if i in second_user.keys():
-                common_movie_ratings.append([first_user[i],second_user[i]])
-
-    return common_movie_ratings
-
-for i in ratings.keys():
-    for j in ratings.keys():
-        if not i==j:
-            common_movie_ratings=find_similar_movie_ratings(ratings[i],ratings[j])
-
-            pass
-
-
+import math
 
 class filter:
 
@@ -58,7 +7,10 @@ class filter:
         self.users_mean_ratings={}
         self.mean_centered_ratings={}
         self.common_ratings_between_users={}
+        self.sum_of_squares_of_ratings_for_users={}
         self.weights_of_users={}
+        self.normalizing_factor={}
+        self.start()
 
     def load_data(self):
         with open('./netflix/TrainingRatings.txt') as f:
@@ -83,7 +35,7 @@ class filter:
             self.users_mean_ratings[i]=sum_of_ratings/count
 
             for j in self.movie_ratings[i].keys():
-                if not i in self.movie_ratings.keys():
+                if not i in self.mean_centered_ratings.keys():
                     self.mean_centered_ratings[i]={}
                 self.mean_centered_ratings[i][j]=self.movie_ratings[i][j]-self.users_mean_ratings[i]
 
@@ -98,6 +50,8 @@ class filter:
                 if i in user2.keys():
                     common_ratings.append([user1[i],user2[i]])
 
+        return common_ratings
+
 
 
     def calculate_weights_of_users(self):
@@ -107,3 +61,46 @@ class filter:
                     if not i in self.common_ratings_between_users.keys():
                         self.common_ratings_between_users[i]={}
                     self.common_ratings_between_users[i][j]=self.find_common_ratings_between_users(self.movie_ratings[i],self.movie_ratings[j])
+
+
+        for i in self.common_ratings_between_users.keys():
+            for j in self.common_ratings_between_users[i].keys():
+                common_ratings=self.common_ratings_between_users[i][j]
+
+                sum_of_product_user1_user2=0
+                sum_of_squares_user1=0
+                sum_of_squares_user2=0
+
+                for k in common_ratings:
+                    sum_of_product_user1_user2+=k[0]*k[1]
+                    sum_of_squares_user1+=k[0]**2
+                    sum_of_squares_user2+=k[1]**2
+                
+                if not j in self.weights_of_users[i]:
+                    self.weights_of_users[i]={}
+                
+                self.weights_of_users[i][j]=sum_of_product_user1_user2/math.sqrt(sum_of_squares_user1*sum_of_squares_user2)
+
+    
+    def calculate_normalizing_factors(self):
+        for i in self.weights_of_users.keys():
+            sum_of_weights=0
+            for j in self.weights_of_users[i].keys():
+                sum_of_weights+=self.weights_of_users[i][j]
+            self.normalizing_factor[i]=sum_of_weights
+
+
+    def start(self):
+        self.load_data()
+        self.calculate_mean_ratings_of_users()
+        self.calculate_weights_of_users()
+        self.calculate_normalizing_factors()
+
+
+
+algo=filter()
+
+print(algo.normalizing_factor)
+
+
+
